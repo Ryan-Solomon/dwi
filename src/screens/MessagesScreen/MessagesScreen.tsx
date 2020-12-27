b3f236ae3f6ceeabfbf2b26dbfc46933cd90070a
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-27 17:03:32 -0600

    add messages screen

---
+++ src/screens/MessagesScreen/MessagesScreen.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { FlatList } from 'react-native';
+import { ListItem } from '../../components/list-item/ListItem';
+
+const fakeMessages = [
+  {
+    id: 1,
+    title: 'T1',
+    description: 'words1',
+    image: require('../../../assets/img/logo-red.png'),
+  },
+  {
+    id: 2,
+    title: 'T2',
+    description: 'words2',
+    image: require('../../../assets/img/logo-red.png'),
+  },
+];
+
+export const MessagesScreen = () => {
+  return (
+    <FlatList
+      data={fakeMessages}
+      keyExtractor={(message) => message.id.toString()}
+      renderItem={({ item }) => {
+        return (
+          <ListItem
+            subtitle={item.description}
+            title={item.title}
+            key={item.id}
+            image={item.image}
+          />
+        );
+      }}
+    />
+  );
+};
