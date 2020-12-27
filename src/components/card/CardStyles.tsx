1c425b4069353be0bb8685818851f8fe39401204
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-27 13:51:33 -0600

    add card styles

--- src/components/card/CardStyles.tsx
+++ src/components/card/CardStyles.tsx
@@ -1,14 +1,11 @@
 import styled from 'styled-components/native';
 
 export const StyledCardContainer = styled.View`
-  width: 100%;
-  margin-top: 30px;
-  margin-right: 30px;
-  margin-bottom: 30px;
-  margin-left: 30px;
+  width: 90%;
+  margin: auto;
   border-radius: 10px;
   overflow: hidden;
-  background-color: blue;
+  background-color: #cbcbcb;
 `;
 
 type StyledTextProps = {
@@ -20,5 +17,6 @@ export const StyledCardText = styled.Text<Partial<StyledTextProps>>`
   color: ${({ fontColor = 'black' }) => fontColor};
   font-size: ${({ fontSize = '16px' }) => fontSize};
   margin-top: 16px;
+  margin-bottom: 10px;
   margin-left: 16px;
 `;
