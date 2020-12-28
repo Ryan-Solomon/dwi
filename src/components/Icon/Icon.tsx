0623c9604b8a887a1e7e821e1c444ee3aeed067b
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-28 14:27:35 -0600

    refactor icon so it works

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { StyledIconContainer } from '../../screens/ViewImageScreen/ViewImageScreenStyles';
+import styled from 'styled-components/native';
 
 type TIconProps = {
   name: string;
@@ -11,17 +11,30 @@ type TIconProps = {
 
 export const Icon: FC<TIconProps> = ({
   name,
-  size = 40,
+  size = 50,
   backgroundColor = 'black',
   iconColor = 'white',
 }) => {
   return (
-    <StyledIconContainer>
+    <StyledIconContainer backgroundColor={backgroundColor} size={size}>
       <MaterialCommunityIcons
         name={name as any}
-        size={size / 2}
+        size={size * 0.5}
         color={iconColor}
       />
     </StyledIconContainer>
   );
 };
+
+type TStyleProps = {
+  backgroundColor: string;
+  size: number;
+};
+
+const StyledIconContainer = styled.View<TStyleProps>`
+  background-color: ${({ backgroundColor }) => backgroundColor};
+  width: ${({ size }) => `${size}px`};
+  height: ${({ size }) => `${size}px`};
+  justify-content: center;
+  align-items: center;
+`;
